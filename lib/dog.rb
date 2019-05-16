0f0ce213a616d6bf17b7d90c0090b4eda56dd484
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-16 00:30:59 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed_type)
+    @breed = breed_type
+  end
+
+  def breed
+    @breed
+  end
+
+end
